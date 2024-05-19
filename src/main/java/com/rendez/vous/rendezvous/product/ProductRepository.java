4fd7961d1d8175e27dad5e22897bde39af9b37ce
Hassan FIGUIGUI <115175070+hassanfigui1@users.noreply.github.com>
2024-05-19 19:45:55 +0100

    Delete src/main/java/com/rendez/vous/rendezvous/product directory

--- src/main/java/com/rendez/vous/rendezvous/product/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.rendez.vous.rendezvous.product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
